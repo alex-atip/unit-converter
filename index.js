ac4c9ee45fcbe139803ad2c7e8a79c0de79f1086
Alexandre <alexandreoliver0212@icloud.com>
2025-11-11 18:43:56 -0500

    feat: add volume and mass conversion logic

--- index.js
+++ index.js
@@ -7,17 +7,19 @@
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const lenParagEl = document.getElementById("len-el")
+const volParagEl = document.getElementById("volume-el")
+const massParagEl = document.getElementById("mass-el")
 
 
 
 inputBtn.addEventListener("click", function() {
   const inputValue = Number(inputEl.value)
+
   const lenResult = len(inputValue)
+  const volResult = volume(inputValue)
+  const massResult = mass(inputValue)
 
-  console.log(lenResult.totalMeter)
-  console.log(lenResult.totalFeet)
-
-  render(inputValue, lenResult)
+  render(inputValue, lenResult, volResult, massResult)
 
   inputEl.value = ""
 })
@@ -32,9 +34,37 @@ function len(inputValue) {
   }
 }
 
-function render(inputValue, lenResult) {
+function volume(inputValue) {
+  const totalGallons = Number(inputValue) * 0.264
+  const totalLiters = Number(inputValue) / 0.264
+  
+  return {
+    totalGallons: totalGallons.toFixed(3),
+    totalLiters: totalLiters.toFixed(3)
+  }
+}
+
+function mass(inputValue) {
+  const totalPounds = Number(inputValue) * 2.204
+  const totalKilos = Number(inputValue) / 2.204
+  
+  return {
+    totalPounds: totalPounds.toFixed(3),
+    totalKilos: totalKilos.toFixed(3)
+  }
+}
+
+function render(inputValue, lenResult, volResult, massResult) {
   lenParagEl.textContent = 
     `${inputValue} meters = ${lenResult.totalMeter} feet | ` +
     `${inputValue} feet = ${lenResult.totalFeet} meters`
+
+  volParagEl.textContent = 
+    `${inputValue} liters = ${volResult.totalGallons} gallons | ` +
+    `${inputValue} gallons = ${volResult.totalLiters} liters`
+
+    massParagEl.textContent = 
+    `${inputValue} kilos = ${massResult.totalPounds} pounds | ` +
+    `${inputValue} pounds = ${massResult.totalKilos} kilos`
 }
 
